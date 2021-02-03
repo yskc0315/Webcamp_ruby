b6644c674197a9c3d42b79de4a09ac96540d0acc
EC2 Default User <ec2-user@ip-172-31-15-160.ec2.internal>
2021-02-03 05:42:32 +0000

    Webcamp_ruby

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '32059fbdb1f9a78143bb38be21d28fcb394f21e1f13a0ec63b24364fef9f2e66661ced577b3ef1e73fb8695e0cd69a2c016f195f756f239eb28defe835e90c53'
+
